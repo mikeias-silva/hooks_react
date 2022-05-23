751fc4db8336caf6b008010307ef054c735a0ff1
mikeias-silva <mikeias26@gmail.com>
2022-05-22 22:43:10 -0300

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import EntradaDado from './components/EntradaDado';
+import SaidaDado from './components/SaidaDado';
 
 function App() {
+  const [username, setUsername] = useState("Maujor");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EntradaDado username={username} setUsername={setUsername}/>
+      <SaidaDado username={username}/>
     </div>
   );
 }
---
+++ src/components/SaidaDado.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SaidaDado = ({username}) => {
+  return (
+    <h1>Seja bem-vindo: {username}</h1>
+  )
+}
+
+export default SaidaDado
